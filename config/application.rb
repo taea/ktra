5d4b163299451d802b35b994274a2fc5f26e3cff
taea <igiari@gmail.com>
2013-04-27 03:11:02 +0900

    Sass settings

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Ktra
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.sass.line_comments = false
+    config.sass.cache = false
   end
 end
