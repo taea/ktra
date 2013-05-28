a8b578ea67ac48d09d71946e4853db770b17cd96
taea <igiari@gmail.com>
2013-05-29 00:28:29 +0900

    Change @task conditions and order
    * Hide done tasks
    * order by status and updated_at

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   # GET /tasks
   # GET /tasks.json
   def index
-    @tasks = Task.all
+    @tasks = Task.find(:all, :conditions => {:status => ['unstarted', 'doing']}, :order => "status ASC, updated_at DESC")
 
     respond_to do |format|
       format.html # index.html.erb
