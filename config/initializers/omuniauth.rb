a8b4875854d1d9b0ce7d81bf3c6b9813f2a08e89
taea <igiari@gmail.com>
2013-08-16 20:42:59 +0900

    Setup for omniauth-twitter

--- config/initializers/omuniauth.rb
+++ config/initializers/omuniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :twitter, Figaro.env.twitter.key, Figaro.env.twitter.secret
+  provider :twitter, Figaro.env.twitter_key, Figaro.env.twitter_secret
 end
