543c926eeb33a4415a47581976617ab8f4375340
taea <igiari@gmail.com>
2013-08-18 06:36:47 +0900

    Find or create users and login with twitter auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def create
-    @user = User.find_or_create_from_auth_hash(auth_hash)
-    # self.current_user = @user
+    @user = User.find_or_create_by_oauth(auth_hash, current_user)
+    session[:user_id] = @user.id
     redirect_to '/'
   end
 
