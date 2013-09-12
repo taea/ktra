1edf3391fd9620f232f71f46d8553697d1a9eea3
koshikawa <koshikawa@ppworks.jp>
2013-09-12 20:36:29 +0900

    Add Iteration#total_point spec

--- spec/models/iteration_spec.rb
+++ spec/models/iteration_spec.rb
@@ -10,4 +10,15 @@ describe Iteration do
   describe 'relations' do
     it { should have_many :tasks }
   end
+
+  describe '#total_point' do
+    subject { iteration.total_point }
+    let(:tasks) { [create(:task), create(:task)] }
+    before do
+      iteration.tasks = tasks
+    end
+    it '合計値が算出できること' do
+      should eq tasks.size
+    end
+  end
 end
