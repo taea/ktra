e387e33da2f114674746b2df8e3c79311a58afa7
takuyan <fanciful.dream@gmail.com>
2014-03-06 21:35:49 +0900

    make private for application helper_methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,11 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :sign_in?
   protect_from_forgery
 
+  private
+
   def current_user
     if session[:user_id]
-      User.find(session[:user_id]) 
+      @user ||= User.find(session[:user_id]) 
     else
       nil
     end
