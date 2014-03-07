9173783cea1edae8b1cb59fdf5597b59f7691ae9
koshikawa <koshikawa@ppworks.jp>
2014-03-07 19:49:12 +0900

    Improve user sigin flow; fixed #22

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,30 +6,23 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :uid
 
   def self.find_or_create_by_oauth(auth, current_user)
-    if user = User.find_by_oauth(auth)
-      user
-    else
-      User.create_by_oauth(auth)
+    begin
+      params = {
+        uid: auth['uid'],
+        name: auth['info']['name'],
+        nickname: auth['info']['nickname'],
+        image: auth['info']['image'],
+        token: auth['credentials']['token'],
+        secret: auth['credentials']['secret'],
+      }
+    rescue => e
+      return nil
     end
-  end
+    # 大きい画像を使っちゃおうね
+    params[:image].gsub!(/_normal/, '_bigger')
 
-  def self.find_by_oauth(auth)
-    user = User.find_by_uid(auth['uid'])
-    if user
-      user
-    else
-      nil
-    end
-  end
-
-  def self.create_by_oauth(auth)
-    user = User.new
-    user.uid = auth['uid']
-    user.name = auth.info['name']
-    user.nickname = auth.info['nickname']
-    user.image = auth.info['image']
-    user.token = auth.credentials['token']
-    user.secret = auth.credentials['secret']
+    user = User.where(uid: params[:uid]).first_or_initialize
+    user.attributes = params
     user.save
     user
   end
