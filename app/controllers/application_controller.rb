b8ce6d40afe34a0607f6753e871458b0646adcab
takuyan <fanciful.dream@gmail.com>
2014-03-07 09:56:18 +0900

    simplify current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,7 @@ class ApplicationController < ActionController::Base
   private
 
   def current_user
-    if session[:user_id] && User.exists?(id: session[:user_id])
-      @user ||= User.find(session[:user_id])
-    else
-      nil
-    end
+    @user ||= User.where(id: session[:user_id]).first
   end
 
   def sign_in?
