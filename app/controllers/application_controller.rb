28b664e6cad0f9139f19ff10b12f4cca61899172
takuyan <fanciful.dream@gmail.com>
2014-03-07 09:15:16 +0900

    if couldnt find by session[:user_id] from User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
   private
 
   def current_user
-    if session[:user_id]
-      @user ||= User.find(session[:user_id]) 
+    if session[:user_id] && User.exists?(id: session[:user_id])
+      @user ||= User.find(session[:user_id])
     else
       nil
     end
