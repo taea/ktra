3565dc825a1e67cff4e378f75a7c1c01286e69fd
taea <igiari@gmail.com>
2014-03-10 12:28:42 +0900

    Refactor; define points in model

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,8 @@ class Task < ActiveRecord::Base
   validates_presence_of :user_id, :title
   belongs_to :user
   scope :active, -> { where(status: ['unstarted', 'doing']).order("status ASC, updated_at DESC") }
+  POINTS = %w(0 1 2 3 5 8)
+  def Task
+    POINTS
+  end
 end
