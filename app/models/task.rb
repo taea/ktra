cd739588c5f1a9c18c3cdb1d61f0b3da94cada6f
taea <igiari@gmail.com>
2014-03-11 03:30:13 +0900

    :high_heel:

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ActiveRecord::Base
+  POINTS = %w(0 1 2 3 5 8)
   validates_presence_of :user_id, :title
   belongs_to :user
   scope :active, -> { where(status: ['unstarted', 'doing']).order("status ASC, updated_at DESC") }
-  POINTS = %w(0 1 2 3 5 8)
 end
