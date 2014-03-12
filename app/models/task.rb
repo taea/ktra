26ca3bbc43b1361150345bd279cf7d596ab31f2c
takuyan <fanciful.dream@gmail.com>
2014-03-12 09:28:57 +0900

    fix total point of iteration

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,5 @@ class Task < ActiveRecord::Base
   validates_presence_of :user_id, :title
   belongs_to :user
   scope :active, -> { where(status: ['unstarted', 'doing']).order("status ASC, updated_at DESC") }
+  scope :owned_by, -> (user) { where(user_id: user ) }
 end
