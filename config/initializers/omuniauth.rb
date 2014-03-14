58da242cf1f073f7fc0027883f700fb0c49abe88
koshikawa <koshikawa@ppworks.jp>
2014-03-14 16:19:54 +0900

    Migrate to devise

--- config/initializers/omuniauth.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :developer unless Rails.env.production?
-  provider :twitter, Figaro.env.twitter_key, Figaro.env.twitter_secret
-end
