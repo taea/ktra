6796161dffc0959f20ca43e11196d679bf67b0be
takuyan <fanciful.dream@gmail.com>
2014-04-11 21:33:45 +0900

    Minimum Javascript set for Non Signin Users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,7 @@ Ktra::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( welcome.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
