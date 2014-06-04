7ce322444a606ddf3cc40c5e0769a456cfa76200
takuyan <fanciful.dream@gmail.com>
2014-06-04 09:19:15 +0900

    Remove Unnecessary :status param, move to StatusesController

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -75,6 +75,6 @@ class TasksController < ApplicationController
   end
 
   def task_param
-    params.require(:task).permit(:finished_at, :memo, :point, :started_at, :status, :title, :week_id)
+    params.require(:task).permit(:finished_at, :memo, :point, :started_at, :title, :week_id)
   end
 end
