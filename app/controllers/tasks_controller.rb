d8cd6035f9e0e6339ae5f08bf4606e7c55bcd5ce
takuyan <fanciful.dream@gmail.com>
2014-06-04 10:04:08 +0900

    Shortened for HoundCI

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -65,11 +65,14 @@ class TasksController < ApplicationController
   end
 
   private
+
   def set_task
     @task = current_user.tasks.find(params[:id])
   end
 
   def task_param
-    params.require(:task).permit(:finished_at, :memo, :point, :started_at, :title, :week_id)
+    params
+      .require(:task)
+      .permit(:finished_at, :memo, :point, :started_at, :title, :week_id)
   end
 end
