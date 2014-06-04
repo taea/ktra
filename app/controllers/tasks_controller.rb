510f192709ef556c2a3001c6230743b96ff85ebb
takuyan <fanciful.dream@gmail.com>
2014-06-04 09:51:34 +0900

    Remove unnecessary html rendering

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -28,11 +28,9 @@ class TasksController < ApplicationController
 
     respond_to do |format|
       if @task.save
-        format.html { redirect_to root_path, notice: 'Task was successfully created.' }
         format.json { render json: @task, status: :created, location: @task }
         format.js { render }
       else
-        format.html { render action: "new" }
         format.json { render json: @task.errors, status: :unprocessable_entity }
       end
     end
@@ -47,11 +45,9 @@ class TasksController < ApplicationController
   def update
     respond_to do |format|
       if @task.update_attributes(task_param)
-        format.html { redirect_to root_path, notice: 'Task was successfully updated.' }
         format.json { head :no_content }
         format.js { render }
       else
-        format.html { render action: "edit" }
         format.json { render json: @task.errors, status: :unprocessable_entity }
       end
     end
@@ -63,7 +59,6 @@ class TasksController < ApplicationController
     @task.destroy
 
     respond_to do |format|
-      format.html { redirect_to tasks_url }
       format.json { head :no_content }
       format.js { render }
     end
